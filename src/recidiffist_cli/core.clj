1cb9e017107550ad2826f2525a5b0f9519651b60
Laurens Van Houtven (lvh) <_@lvh.io>
2018-12-26 15:12:17 -0600

    Make it work on Graal

--- src/recidiffist_cli/core.clj
+++ src/recidiffist_cli/core.clj
@@ -3,9 +3,16 @@
             [clojure.java.io :as io]
             [cheshire.core :as json]
             [recidiffist.stream :as rs]
-            [manifold.stream :as ms])
+            [manifold.stream :as ms]
+            [clojure.data :as cd])
   (:gen-class))
 
+;; This disables Java array diffing. We don't care about that anyway but _also_
+;; confuses GraalVM native reflection. See original implementation for details:
+;; https://github.com/clojure/clojure/blob/9baebd091b1301aecaaac3b5f9c7ede5dcc58f8c/src/clj/clojure/data.clj#L79
+(extend Object
+  cd/Diff {:diff-similar @#'cd/atom-diff}
+  cd/EqualityPartition {:equality-partition (constantly :atom)})
 
 (defn -main
   [& args]
